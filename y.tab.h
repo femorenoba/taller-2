0d5948e9f6d24f246217f8f038e2dd866d514693
Cristian Camilo Vargas Morales <42346462+crvargasm@users.noreply.github.com>
2022-02-06 19:50:06 -0500

    Fix Merge

--- y.tab.h
+++ y.tab.h
@@ -73,12 +73,11 @@
      UNIONMODELO = 289,
      UNIONTRATAMIENTO = 290,
      CONCATENACIONVECTORES = 291,
-     CONCATENADORB = 292,
-     BUSCARVECTOR = 293,
-     ID = 294,
-     NUMERO_ENTERO = 295,
-     NUMERO_FLOTANTE = 296,
-     LOGICA = 297
+     BUSCARVECTOR = 292,
+     ID = 293,
+     NUMERO_ENTERO = 294,
+     NUMERO_FLOTANTE = 295,
+     LOGICA = 296
    };
 #endif
 /* Tokens.  */
@@ -116,12 +115,11 @@
 #define UNIONMODELO 289
 #define UNIONTRATAMIENTO 290
 #define CONCATENACIONVECTORES 291
-#define CONCATENADORB 292
-#define BUSCARVECTOR 293
-#define ID 294
-#define NUMERO_ENTERO 295
-#define NUMERO_FLOTANTE 296
-#define LOGICA 297
+#define BUSCARVECTOR 292
+#define ID 293
+#define NUMERO_ENTERO 294
+#define NUMERO_FLOTANTE 295
+#define LOGICA 296
 
 
 
@@ -131,7 +129,7 @@ typedef union YYSTYPE
 {
 
 /* Line 1676 of yacc.c  */
-#line 125 "Rachas.y"
+#line 124 "Rachas.y"
 
     char* str;
     int numberI;
@@ -144,7 +142,7 @@ typedef union YYSTYPE
 
 
 /* Line 1676 of yacc.c  */
-#line 148 "y.tab.h"
+#line 146 "y.tab.h"
 } YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
